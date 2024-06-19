fb2478d34e5a07a1caf14b0320c8fad4043a4ddc
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-06-19 13:23:26 +0200

    serializable

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/domain/Attestasjonsdata.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/domain/Attestasjonsdata.kt
@@ -1,5 +1,8 @@
 package no.nav.sokos.oppdrag.attestasjon.domain
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Attestasjonsdata(
     val kode_faggruppe: String,
     val navn_faggruppe: String,
