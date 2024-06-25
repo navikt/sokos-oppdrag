6a6c3fc1c2134af87dd4021406dfc321060d2dc6
jonasnico <jonas.nicolaysen@nav.no>
2024-06-25 12:58:49 +0200

    lagt til en enkel test for attestasjonsapi'et

--- src/test/kotlin/no/nav/sokos/oppdrag/TestUtil.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/TestUtil.kt
@@ -5,6 +5,7 @@ import no.nav.sokos.oppdrag.config.PropertiesConfig
 
 const val APPLICATION_JSON = "application/json"
 const val OPPDRAGSINFO_BASE_API_PATH = "/api/v1/oppdragsinfo"
+const val ATTESTASJON_BASE_API_PATH = "/api/v1/attestasjon"
 
 object TestUtil {
     private fun String.readFromResource() = {}::class.java.classLoader.getResource(this)!!.readText()
