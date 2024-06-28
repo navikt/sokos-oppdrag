9f4bea039399648380949b7536ba2b7cae810e4e
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-06-28 13:27:42 +0200

    Tar med andre parametre i sok

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
@@ -71,28 +71,84 @@ class AttestasjonRepository(
             session.list(
                 queryOf(
                     """
-                    select
-                        oppdragslinje.kode_klasse     as kode,
-                        oppdragslinje.delytelse_id    as delytelse_id,
-                        oppdragslinje.sats            as sats,
-                        oppdragslinje.type_sats       as type_sats,
-                        oppdragslinje.dato_vedtak_fom as dato_vedtak_fom,
-                        l2.dato_vedtak_fom - 1 day    as dato_vedtak_tom,
-                        oppdragslinje.attestert       as attestert,
-                        attestasjon.attestant_id      as attestant_id,
-                        oppdrag.fagsystem_id          as fagsystem_id,
-                        fagomraade.navn_fagomraade    as navn_fagomraade
-                    from t_oppdragslinje oppdragslinje
-                             left outer join T_KORREKSJON k on oppdragslinje.OPPDRAGS_ID = k.OPPDRAGS_ID and oppdragslinje.LINJE_ID = k.LINJE_ID
-                             left outer join T_OPPDRAGSLINJE l2 on l2.OPPDRAGS_ID = oppdragslinje.OPPDRAGS_ID and l2.LINJE_ID = k.LINJE_ID_KORR
-                             join t_oppdrag oppdrag on oppdrag.oppdrags_id = oppdragslinje.oppdrags_id
-                             join t_fagomraade fagomraade on fagomraade.kode_fagomraade = oppdrag.kode_fagomraade
-                             left outer join t_attestasjon attestasjon
-                                             on attestasjon.oppdrags_id = oppdragslinje.oppdrags_id
-                                                 and attestasjon.linje_id = oppdragslinje.linje_id
-                    where 1=1
-                        and (l2.DATO_VEDTAK_FOM is null or l2.DATO_VEDTAK_FOM > oppdragslinje.DATO_VEDTAK_FOM)
-                        and oppdragslinje.oppdrags_id = :oppdragsId
+                    select o.oppdrags_id
+                         , l.linje_id
+                         , o.oppdrag_gjelder_id
+                         , g.navn_faggruppe
+                         , f.navn_fagomraade
+                         , o.kode_fagomraade
+                         , o.fagsystem_id
+                         , l.kode_klasse
+                         , l.delytelse_id
+                         , l.sats
+                         , l.type_sats
+                         , l.dato_vedtak_fom
+                         , l.dato_vedtak_tom
+                         , ls.kode_status
+                         , a.attestant_id
+                         , a.dato_ugyldig_fom
+                      from t_faggruppe g join t_fagomraade f on g.kode_faggruppe = f.kode_faggruppe
+                      join t_oppdrag o on f.kode_fagomraade = o.kode_fagomraade
+                      join t_oppdragslinje l on o.oppdrags_id = l.oppdrags_id
+                      join t_oppdrag_status s on s.oppdrags_id = l.oppdrags_id
+                      join t_linje_status ls on ls.oppdrags_id = l.oppdrags_id and ls.linje_id = l.linje_id
+                      left join t_korreksjon k on l.oppdrags_id = k.oppdrags_id and l.linje_id = k.linje_id
+                      left join t_attestasjon a on a.oppdrags_id = l.oppdrags_id and a.linje_id = l.linje_id
+                    where k.oppdrags_id is null
+                      and s.kode_status = 'AKTI'
+                      and s.tidspkt_reg = (select max(s2.tidspkt_reg)
+                                             from t_oppdrag_status s2
+                                            where s.oppdrags_id = s2.oppdrags_id)
+                      and ls.tidspkt_reg = (select max(tidspkt_reg)
+                                             from t_linje_status ls2
+                                            where ls2.oppdrags_id = ls.oppdrags_id
+                                              and ls2.linje_id = ls.linje_id)
+                      and (a.oppdrags_id is null or a.lopenr = (select max(a2.lopenr)
+                                                                  from t_attestasjon a2
+                                                                 where a2.oppdrags_id = l.oppdrags_id
+                                                                   and a2.linje_id = l.linje_id
+                                                                   and a2.attestant_id = a.attestant_id))
+                      and o.oppdrags_id  = :oppdragsId
+                    union
+                    select o.oppdrags_id
+                        , l.linje_id
+                        , o.oppdrag_gjelder_id
+                        , g.navn_faggruppe
+                        , f.navn_fagomraade
+                        , o.kode_fagomraade
+                        , o.fagsystem_id
+                        , l.kode_klasse
+                        , l.delytelse_id
+                        , l.sats
+                        , l.type_sats
+                        , l.dato_vedtak_fom
+                        , l2.dato_vedtak_fom - 1 day as dato_vedtak_tom
+                        , ls.kode_status
+                        , a.attestant_id
+                        , a.dato_ugyldig_fom
+                     from t_faggruppe g join t_fagomraade f on g.kode_faggruppe = f.kode_faggruppe
+                     join t_oppdrag o on f.kode_fagomraade = o.kode_fagomraade
+                     join t_oppdragslinje l on o.oppdrags_id = l.oppdrags_id
+                     join t_oppdrag_status s on s.oppdrags_id = l.oppdrags_id
+                     join t_linje_status ls on ls.oppdrags_id = l.oppdrags_id and ls.linje_id = l.linje_id
+                     join t_korreksjon k on l.oppdrags_id = k.oppdrags_id and l.linje_id = k.linje_id
+                     join t_oppdragslinje l2 on l2.oppdrags_id = k.oppdrags_id_korr and l2.linje_id = k.linje_id_korr and l.dato_vedtak_fom < l2.dato_vedtak_fom
+                     left join t_attestasjon a on a.oppdrags_id = l.oppdrags_id and a.linje_id = l.linje_id
+                    where s.kode_status = 'AKTI'
+                      and s.tidspkt_reg = (select max(s2.tidspkt_reg)
+                                             from t_oppdrag_status s2
+                                            where s.oppdrags_id = s2.oppdrags_id)
+                      and ls.tidspkt_reg = (select max(tidspkt_reg)
+                                             from t_linje_status ls2
+                                            where ls2.oppdrags_id = ls.oppdrags_id
+                                              and ls2.linje_id = ls.linje_id)
+                      and (a.oppdrags_id is null or a.lopenr = (select max(a2.lopenr)
+                                                                  from t_attestasjon a2
+                                                                 where a2.oppdrags_id = l.oppdrags_id
+                                                                   and a2.linje_id = l.linje_id
+                                                                   and a2.attestant_id = a.attestant_id))
+                      and o.oppdrags_id  = :oppdragsId
+                    order by oppdrags_id, LINJE_ID
                     """.trimIndent(),
                     mapOf("oppdragsId" to oppdragsId),
                 ),
@@ -112,13 +168,12 @@ class AttestasjonRepository(
 
     private val mapToOppdragslinjerTilAttestasjon: (Row) -> Attestasjonsdetaljer = { row ->
         Attestasjonsdetaljer(
-            klasse = row.string("kode").trim(),
+            klasse = row.string("kode_klasse").trim(),
             delytelsesId = row.string("delytelse_id").trim(),
             sats = row.double("sats"),
             satstype = row.string("type_sats").trim(),
             datoVedtakFom = row.string("dato_vedtak_fom").trim(),
             datoVedtakTom = row.stringOrNull("dato_vedtak_tom")?.trim(),
-            attestert = row.string("attestert").trim(),
             attestant = row.string("attestant_id").trim(),
             fagsystemId = row.string("fagsystem_id").trim(),
             navnFagomraade = row.string("navn_fagomraade").trim(),
