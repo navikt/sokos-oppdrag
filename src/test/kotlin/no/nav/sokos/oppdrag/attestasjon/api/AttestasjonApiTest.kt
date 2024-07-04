551be25dc18256284bb72ced4e920aad600a5736
jonasnico <jonas.nicolaysen@nav.no>
2024-07-04 11:33:17 +0200

    fikset swagger og en liten test

--- src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/api/AttestasjonApiTest.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/api/AttestasjonApiTest.kt
@@ -13,13 +13,11 @@ import io.ktor.server.routing.routing
 import io.mockk.every
 import io.mockk.mockk
 import io.restassured.RestAssured
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.serializer
 import no.nav.sokos.oppdrag.APPLICATION_JSON
 import no.nav.sokos.oppdrag.ATTESTASJON_BASE_API_PATH
 import no.nav.sokos.oppdrag.TestUtil.tokenWithNavIdent
 import no.nav.sokos.oppdrag.attestasjon.domain.AttestasjonTreff
+import no.nav.sokos.oppdrag.attestasjon.domain.Attestasjonsdetaljer
 import no.nav.sokos.oppdrag.attestasjon.model.AttestasjondetaljerRequestBody
 import no.nav.sokos.oppdrag.attestasjon.service.AttestasjonService
 import no.nav.sokos.oppdrag.common.model.GjelderIdRequestBody
@@ -69,11 +67,34 @@ internal class AttestasjonApiTest : FunSpec({
         response.body.jsonPath().getList<AttestasjonTreff>("oppdragsId").first().shouldBe(987654)
     }
 
-    test("foo") {
-        val attestasjondetaljerRequest = AttestasjondetaljerRequestBody(listOf(1, 2, 3))
+    test("søk etter oppdragsId på oppdragslinjer endepunktet skal returnere 200 OK") {
+        val attestasjonsdetaljer =
+            Attestasjonsdetaljer(
+                klasse = "klasse",
+                delytelsesId = "delytelsesId",
+                sats = 123.45,
+                satstype = "satstype",
+                datoVedtakFom = "2021-01-01",
+                datoVedtakTom = "2021-12-31",
+                attestant = "attestant",
+                navnFagomraade = "navnFagomraade",
+                fagsystemId = "123456789",
+            )
 
-        val jsonString = Json.encodeToString(AttestasjondetaljerRequestBody.serializer(), attestasjondetaljerRequest)
-        println(jsonString)
+        val attestasjonsdetaljerliste = listOf(attestasjonsdetaljer)
+
+        every { attestasjonService.hentListeMedOppdragslinjerForAttestering(any()) } returns attestasjonsdetaljerliste
+
+        val response =
+            RestAssured.given().filter(validationFilter).header(HttpHeaders.ContentType, APPLICATION_JSON)
+                .header(HttpHeaders.Authorization, "Bearer $tokenWithNavIdent")
+                .body(AttestasjondetaljerRequestBody(listOf(987654)))
+                .port(PORT)
+                .post("$ATTESTASJON_BASE_API_PATH/oppdragslinjer").then().assertThat()
+                .statusCode(HttpStatusCode.OK.value)
+                .extract().response()
+
+        response.body.jsonPath().getList<Int>("fagsystemId").first().shouldBe("123456789")
     }
 })
 
