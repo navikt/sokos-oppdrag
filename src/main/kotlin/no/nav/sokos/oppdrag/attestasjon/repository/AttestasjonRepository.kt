960c7f0a9589bf21f1cc55dd025182fd87a6250e
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-08-13 13:35:43 +0200

    Endrer spørring for Detaljer

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
@@ -101,11 +101,11 @@ class AttestasjonRepository(
             session.list(
                 queryOf(
                     """
-                    select o.oppdrags_id
+                         select o.oppdrags_id
                          , l.linje_id
                          , o.oppdrag_gjelder_id
-                         , g.navn_faggruppe
-                         , TRIM(f.navn_fagomraade) AS navn_fagomraade
+                         , TRIM(f.kode_faggruppe) AS kode_faggruppe
+                         , TRIM(o.kode_fagomraade) AS kode_fagomraade
                          , o.kode_fagomraade
                          , TRIM(o.fagsystem_id) AS fagsystem_id
                          , TRIM(l.kode_klasse) AS kode_klasse
@@ -113,73 +113,59 @@ class AttestasjonRepository(
                          , l.sats
                          , TRIM(l.type_sats) AS type_sats
                          , l.dato_vedtak_fom
-                         , l.dato_vedtak_tom
-                         , ls.kode_status
-                         , TRIM(a.attestant_id) AS attestant_id
+                         , coalesce(l2.dato_vedtak_fom - 1 day, l.dato_vedtak_tom) as dato_vedtak_tom
+                         , TRIM(a.attestant_id) AS attestand_id
                          , a.dato_ugyldig_fom
-                      from t_faggruppe g join t_fagomraade f on g.kode_faggruppe = f.kode_faggruppe
-                      join t_oppdrag o on f.kode_fagomraade = o.kode_fagomraade
-                      join t_oppdragslinje l on o.oppdrags_id = l.oppdrags_id
-                      join t_oppdrag_status s on s.oppdrags_id = l.oppdrags_id
-                      join t_linje_status ls on ls.oppdrags_id = l.oppdrags_id and ls.linje_id = l.linje_id
-                      left join t_korreksjon k on l.oppdrags_id = k.oppdrags_id and l.linje_id = k.linje_id
-                      left join t_attestasjon a on a.oppdrags_id = l.oppdrags_id and a.linje_id = l.linje_id
-                    where k.oppdrags_id is null
-                      and s.kode_status = 'AKTI'
-                      and s.tidspkt_reg = (select max(s2.tidspkt_reg)
-                                             from t_oppdrag_status s2
-                                            where s.oppdrags_id = s2.oppdrags_id)
-                      and ls.tidspkt_reg = (select max(tidspkt_reg)
-                                             from t_linje_status ls2
-                                            where ls2.oppdrags_id = ls.oppdrags_id
-                                              and ls2.linje_id = ls.linje_id)
-                      and (a.oppdrags_id is null or a.lopenr = (select max(a2.lopenr)
-                                                                  from t_attestasjon a2
-                                                                 where a2.oppdrags_id = l.oppdrags_id
-                                                                   and a2.linje_id = l.linje_id
-                                                                   and a2.attestant_id = a.attestant_id))
-                      and o.oppdrags_id  IN (${oppdragsIder.joinToString()})
-                    union
-                    select o.oppdrags_id
-                        , l.linje_id
-                        , o.oppdrag_gjelder_id
-                        , TRIM(g.navn_faggruppe) AS navn_faggruppe
-                        , TRIM(f.navn_fagomraade) AS navn_fagomraade
-                        , o.kode_fagomraade
-                        , TRIM(o.fagsystem_id) AS fagsystem_id
-                        , TRIM(l.kode_klasse) AS kode_klasse
-                        , TRIM(l.delytelse_id) AS delytelse_id
-                        , l.sats
-                        , TRIM(l.type_sats) AS type_sats
-                        , l.dato_vedtak_fom
-                        , l2.dato_vedtak_fom - 1 day as dato_vedtak_tom
-                        , ls.kode_status
-                        , TRIM(a.attestant_id) AS attestant_id
-                        , a.dato_ugyldig_fom
-                     from t_faggruppe g join t_fagomraade f on g.kode_faggruppe = f.kode_faggruppe
-                     join t_oppdrag o on f.kode_fagomraade = o.kode_fagomraade
-                     join t_oppdragslinje l on o.oppdrags_id = l.oppdrags_id
-                     join t_oppdrag_status s on s.oppdrags_id = l.oppdrags_id
-                     join t_linje_status ls on ls.oppdrags_id = l.oppdrags_id and ls.linje_id = l.linje_id
-                     join t_korreksjon k on l.oppdrags_id = k.oppdrags_id and l.linje_id = k.linje_id
-                     join t_oppdragslinje l2 on l2.oppdrags_id = k.oppdrags_id_korr and l2.linje_id = k.linje_id_korr and l.dato_vedtak_fom < l2.dato_vedtak_fom
-                     left join t_attestasjon a on a.oppdrags_id = l.oppdrags_id and a.linje_id = l.linje_id
+                         , TRIM(oe.enhet) AS kostnadssted
+                         , case when le.enhet is not null then TRIM(le.enhet)
+                                when oeb.enhet is not null then TRIM(oeb.enhet)
+                                else TRIM(oe.enhet)
+                        end as ansvarssted
+                    from t_fagomraade f
+                             join t_oppdrag o on f.kode_fagomraade = o.kode_fagomraade
+                             join t_oppdragslinje l on o.oppdrags_id = l.oppdrags_id
+                             join t_oppdrag_status s on s.oppdrags_id = l.oppdrags_id
+                             join t_linje_status ls on ls.oppdrags_id = l.oppdrags_id and ls.linje_id = l.linje_id
+                             join t_oppdragsenhet oe on oe.oppdrags_id = o.oppdrags_id and oe.type_enhet = 'BOS'
+                             join T_STATUSKODE sk on sk.KODE_STATUS = ls.KODE_STATUS
+                             left join t_korreksjon k on l.oppdrags_id = k.oppdrags_id and l.linje_id = k.linje_id
+                             left join t_oppdrag o2 on k.oppdrags_id_korr = o2.oppdrags_id and f.kode_fagomraade = o2.kode_fagomraade
+                             left join t_oppdragslinje l2 on l2.oppdrags_id = o2.oppdrags_id and k.linje_id_korr = l2.linje_id and l.dato_vedtak_fom < l2.dato_vedtak_fom
+                             left join t_attestasjon a on a.oppdrags_id = l.oppdrags_id and a.linje_id = l.linje_id
+                             left outer join t_linjeenhet le on le.oppdrags_id = l.oppdrags_id and le.type_enhet = 'BEH' and le.linje_id = l.linje_id
+                             left outer join t_oppdragsenhet oeb on oeb.oppdrags_id = o.oppdrags_id and oeb.type_enhet = 'BEH'
                     where s.kode_status = 'AKTI'
+                      and not (l2.DATO_VEDTAK_FOM is null and sk.TYPE_STATUS != 'AKTI')
                       and s.tidspkt_reg = (select max(s2.tidspkt_reg)
-                                             from t_oppdrag_status s2
-                                            where s.oppdrags_id = s2.oppdrags_id)
+                                           from t_oppdrag_status s2
+                                           where s.oppdrags_id = s2.oppdrags_id)
                       and ls.tidspkt_reg = (select max(tidspkt_reg)
-                                             from t_linje_status ls2
+                                            from t_linje_status ls2
                                             where ls2.oppdrags_id = ls.oppdrags_id
                                               and ls2.linje_id = ls.linje_id)
                       and (a.oppdrags_id is null or a.lopenr = (select max(a2.lopenr)
-                                                                  from t_attestasjon a2
-                                                                 where a2.oppdrags_id = l.oppdrags_id
-                                                                   and a2.linje_id = l.linje_id
-                                                                   and a2.attestant_id = a.attestant_id))
+                                                                from t_attestasjon a2
+                                                                where a2.oppdrags_id = l.oppdrags_id
+                                                                  and a2.linje_id = l.linje_id))
+                      and oe.tidspkt_reg = (select max(tidspkt_reg)
+                                            from t_oppdragsenhet oe2
+                                            where oe2.oppdrags_id = oe.oppdrags_id
+                                              and oe2.type_enhet  = oe.type_enhet
+                                              and oe2.dato_fom   <= current date)
+                      and (le.tidspkt_reg is null or le.tidspkt_reg = (select max(tidspkt_reg)
+                                                                       from t_linjeenhet le2
+                                                                       where le2.oppdrags_id = le.oppdrags_id
+                                                                         and le2.linje_id    = le.linje_id
+                                                                         and le2.type_enhet  = le.type_enhet
+                                                                         and le2.dato_fom   <= current date))
+                      and (oeb.tidspkt_reg is null or oeb.tidspkt_reg = (select max(tidspkt_reg)
+                                                                         from t_oppdragsenhet oeb2
+                                                                         where oeb2.oppdrags_id = oeb.oppdrags_id
+                                                                           and oeb2.type_enhet  = oeb.type_enhet
+                                                                           and oeb2.dato_fom   <= current date))
                       and o.oppdrags_id  IN (${oppdragsIder.joinToString()})
                     order by oppdrags_id, LINJE_ID
-                    """.trimIndent(),
+                    """.trimIndent()
                 ),
                 mapToOppdragslinjerTilAttestasjon,
             )
