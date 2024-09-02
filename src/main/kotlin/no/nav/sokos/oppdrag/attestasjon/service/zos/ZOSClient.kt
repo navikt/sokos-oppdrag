4faa68f90a07f00fb98dea14a71c448ddf53d416
mudasar187 <mudasar_cheema@hotmail.com>
2024-09-02 10:31:57 +0200

    legger til method i header mot zos

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/service/zos/ZOSClient.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/service/zos/ZOSClient.kt
@@ -8,6 +8,7 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import io.ktor.http.isSuccess
@@ -31,6 +32,7 @@ class ZOSKlient(
                 header("Nav-Call-Id", MDC.get("x-correlation-id"))
                 accept(ContentType.Application.Json)
                 contentType(ContentType.Application.Json)
+                method = HttpMethod.Post
                 setBody(mapToZosRequest(attestasjonRequest))
             }
 
