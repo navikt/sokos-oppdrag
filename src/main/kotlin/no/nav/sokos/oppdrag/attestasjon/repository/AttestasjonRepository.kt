adef3d773ce4d15f6ca642f41e851e85cb4183a7
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-09-11 15:42:22 +0200

    Begynner med testdrevet implementasjon av ny spørring

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
@@ -7,7 +7,7 @@ import kotliquery.queryOf
 import kotliquery.sessionOf
 import no.nav.sokos.oppdrag.attestasjon.domain.FagOmraade
 import no.nav.sokos.oppdrag.attestasjon.domain.Oppdrag
-import no.nav.sokos.oppdrag.attestasjon.domain.OppdragsDetaljer
+import no.nav.sokos.oppdrag.attestasjon.domain.OppdragslinjeWithoutFluff
 import no.nav.sokos.oppdrag.config.DatabaseConfig
 
 class AttestasjonRepository(
@@ -101,91 +101,39 @@ class AttestasjonRepository(
         }
     }
 
-    fun getOppdragsDetaljer(oppdragsId: Int): List<OppdragsDetaljer> {
+    fun getOppdragslinjerWithoutFluff(oppdragsId: Int): List<OppdragslinjeWithoutFluff> {
+        val query =
+            """
+            select l.OPPDRAGS_ID, l.LINJE_ID, l.KODE_KLASSE, DATO_VEDTAK_FOM, DATO_VEDTAK_TOM, ATTESTERT, l.SATS, l.TYPE_SATS, l.DELYTELSE_ID
+            from T_OPPDRAGSLINJE l
+                     join T_LINJE_STATUS statusNy on statusNy.LINJE_ID = l.LINJE_ID and statusNy.OPPDRAGS_ID = l.OPPDRAGS_ID
+            where statusNy.KODE_STATUS = 'NY'
+              and not exists(select korrannuopph.TIDSPKT_REG
+                             from T_LINJE_STATUS korrannuopph
+                             where korrannuopph.LINJE_ID = l.LINJE_ID
+                               and korrannuopph.OPPDRAGS_ID = l.OPPDRAGS_ID
+                               and korrannuopph.KODE_STATUS in ('KORR', 'ANNU', 'OPPH')
+                               and korrannuopph.DATO_FOM = statusNy.DATO_FOM
+                               and not exists(select 1
+                                              from T_LINJE_STATUS andreStatuser
+                                              where andreStatuser.LINJE_ID = l.LINJE_ID
+                                                and andreStatuser.OPPDRAGS_ID = l.OPPDRAGS_ID
+                                                and andreStatuser.KODE_STATUS in ('IKAT', 'ATTE', 'HVIL', 'REAK', 'FBER', 'LOPE')
+                                                and andreStatuser.DATO_FOM >= statusNy.DATO_FOM
+                                                and andreStatuser.TIDSPKT_REG > korrannuopph.TIDSPKT_REG))
+              and l.OPPDRAGS_ID = :OPPDRAGSID
+            order by KODE_KLASSE, LINJE_ID, DATO_VEDTAK_FOM;
+            """.trimIndent()
+
         return using(sessionOf(dataSource)) { session ->
             session.list(
                 queryOf(
-                    """
-                    SELECT O.OPPDRAGS_ID                                            AS OPPDRAGS_ID
-                         , F.ANT_ATTESTANTER                                        AS ANT_ATTESTANTER
-                         , L.LINJE_ID                                               AS LINJE_ID
-                         , O.OPPDRAG_GJELDER_ID                                     AS OPPDRAG_GJELDER_ID
-                         , TRIM(G.NAVN_FAGGRUPPE)                                   AS NAVN_FAGGRUPPE
-                         , TRIM(F.NAVN_FAGOMRAADE)                                  AS NAVN_FAGOMRAADE
-                         , TRIM(O.KODE_FAGOMRAADE)                                  AS KODE_FAGOMRAADE
-                         , TRIM(O.FAGSYSTEM_ID)                                     AS FAGSYSTEM_ID
-                         , TRIM(L.KODE_KLASSE)                                      AS KODE_KLASSE
-                         , TRIM(L.DELYTELSE_ID)                                     AS DELYTELSE_ID
-                         , L.SATS                                                   AS SATS
-                         , TRIM(L.TYPE_SATS)                                        AS TYPE_SATS
-                         , L.DATO_VEDTAK_FOM                                        AS DATO_VEDTAK_FOM
-                         , COALESCE(L2.DATO_VEDTAK_FOM - 1 DAY, L.DATO_VEDTAK_TOM)  AS DATO_VEDTAK_TOM
-                         , TRIM(A.ATTESTANT_ID)                                     AS ATTESTANT_ID
-                         , A.DATO_UGYLDIG_FOM                                       AS DATO_UGYLDIG_FOM
-                         , TRIM(OKS.ENHET)                                          AS KOSTNADSSTEDFOROPPDRAG
-                         , TRIM(OAS.ENHET)                                          AS ANSVARSSTEDFOROPPDRAG
-                         , TRIM(LKS.ENHET)                                          AS KOSTNADSSTEDFORLINJE
-                         , TRIM(LAS.ENHET)                                          AS ANSVARSSTEDFORLINJE
-                    FROM T_FAGGRUPPE G
-                             JOIN T_FAGOMRAADE F ON G.KODE_FAGGRUPPE = F.KODE_FAGGRUPPE
-                             JOIN T_OPPDRAG O ON F.KODE_FAGOMRAADE = O.KODE_FAGOMRAADE
-                             JOIN T_OPPDRAGSLINJE L ON O.OPPDRAGS_ID = L.OPPDRAGS_ID
-                             JOIN T_OPPDRAG_STATUS S ON S.OPPDRAGS_ID = L.OPPDRAGS_ID
-                             JOIN T_LINJE_STATUS LS ON LS.OPPDRAGS_ID = L.OPPDRAGS_ID AND LS.LINJE_ID = L.LINJE_ID
-                             JOIN T_OPPDRAGSENHET OKS ON OKS.OPPDRAGS_ID = O.OPPDRAGS_ID AND OKS.TYPE_ENHET = 'BOS'
-                             JOIN T_STATUSKODE SK ON SK.KODE_STATUS = LS.KODE_STATUS
-                             LEFT JOIN T_KORREKSJON K ON L.OPPDRAGS_ID = K.OPPDRAGS_ID AND L.LINJE_ID = K.LINJE_ID
-                             LEFT JOIN T_OPPDRAG O2 ON K.OPPDRAGS_ID_KORR = O2.OPPDRAGS_ID AND F.KODE_FAGOMRAADE = O2.KODE_FAGOMRAADE
-                             LEFT JOIN T_OPPDRAGSLINJE L2 ON L2.OPPDRAGS_ID = O2.OPPDRAGS_ID AND K.LINJE_ID_KORR = L2.LINJE_ID AND L.DATO_VEDTAK_FOM < L2.DATO_VEDTAK_FOM
-                             LEFT JOIN T_ATTESTASJON A ON A.OPPDRAGS_ID = L.OPPDRAGS_ID AND A.LINJE_ID = L.LINJE_ID AND A.DATO_UGYLDIG_FOM > CURRENT DATE AND L.ATTESTERT = 'J'
-                             LEFT OUTER JOIN T_LINJEENHET LKS ON LKS.OPPDRAGS_ID = L.OPPDRAGS_ID AND LKS.LINJE_ID = L.LINJE_ID AND LKS.TYPE_ENHET = 'BOS' 
-                             LEFT OUTER JOIN T_LINJEENHET LAS ON LAS.OPPDRAGS_ID = L.OPPDRAGS_ID AND LAS.LINJE_ID = L.LINJE_ID AND LAS.TYPE_ENHET = 'BEH' 
-                             LEFT OUTER JOIN T_OPPDRAGSENHET OAS ON OAS.OPPDRAGS_ID = O.OPPDRAGS_ID AND OAS.TYPE_ENHET = 'BEH'
-                    WHERE S.KODE_STATUS = 'AKTI'
-                      AND NOT (L2.DATO_VEDTAK_FOM IS NULL AND SK.TYPE_STATUS != 'AKTI')
-                      AND S.TIDSPKT_REG = (SELECT MAX(S2.TIDSPKT_REG)
-                                           FROM T_OPPDRAG_STATUS S2
-                                           WHERE S.OPPDRAGS_ID = S2.OPPDRAGS_ID)
-                      AND LS.TIDSPKT_REG = (SELECT MAX(TIDSPKT_REG)
-                                            FROM T_LINJE_STATUS LS2
-                                            WHERE LS2.OPPDRAGS_ID = LS.OPPDRAGS_ID
-                                              AND LS2.LINJE_ID = LS.LINJE_ID)
-                      AND (A.OPPDRAGS_ID IS NULL OR A.LOPENR =          (SELECT MAX(A2.LOPENR)
-                                                                          FROM T_ATTESTASJON A2
-                                                                          WHERE A2.OPPDRAGS_ID = L.OPPDRAGS_ID
-                                                                          AND A2.LINJE_ID = L.LINJE_ID
-                                                                          AND L.ATTESTERT = 'J'
-                                                                          AND A2.ATTESTANT_ID = A.ATTESTANT_ID))
-                      AND OKS.TIDSPKT_REG =                             (SELECT MAX(TIDSPKT_REG)
-                                                                         FROM T_OPPDRAGSENHET OKS2
-                                                                         WHERE OKS2.OPPDRAGS_ID = OKS.OPPDRAGS_ID
-                                                                           AND OKS2.TYPE_ENHET  = OKS.TYPE_ENHET
-                                                                           AND OKS2.DATO_FOM   <= CURRENT DATE)
-                      AND (LKS.TIDSPKT_REG IS NULL OR LKS.TIDSPKT_REG = (SELECT MAX(TIDSPKT_REG)
-                                                                         FROM T_LINJEENHET LKS2
-                                                                         WHERE LKS2.OPPDRAGS_ID = LKS.OPPDRAGS_ID
-                                                                           AND LKS2.LINJE_ID    = LKS.LINJE_ID
-                                                                           AND LKS2.TYPE_ENHET  = LKS.TYPE_ENHET
-                                                                           AND LKS2.DATO_FOM   <= CURRENT DATE))
-                      AND (LAS.TIDSPKT_REG IS NULL OR LAS.TIDSPKT_REG = (SELECT MAX(TIDSPKT_REG)
-                                                                         FROM T_LINJEENHET LAS2
-                                                                         WHERE LAS2.OPPDRAGS_ID = LAS.OPPDRAGS_ID
-                                                                           AND LAS2.LINJE_ID    = LAS.LINJE_ID
-                                                                           AND LAS2.TYPE_ENHET  = LAS.TYPE_ENHET
-                                                                           AND LAS2.DATO_FOM   <= CURRENT DATE))
-                      AND (OAS.TIDSPKT_REG IS NULL OR OAS.TIDSPKT_REG = (SELECT MAX(TIDSPKT_REG)
-                                                                         FROM T_OPPDRAGSENHET OAS2
-                                                                         WHERE OAS2.OPPDRAGS_ID = OAS.OPPDRAGS_ID
-                                                                           AND OAS2.TYPE_ENHET  = OAS.TYPE_ENHET
-                                                                           AND OAS2.DATO_FOM   <= CURRENT DATE))
-                      AND O.OPPDRAGS_ID = :OPPDRAGSID
-                    ORDER BY OPPDRAGS_ID, LINJE_ID
-                    """.trimIndent(),
+                    query,
                     mapOf(
                         "OPPDRAGSID" to oppdragsId,
                     ),
                 ),
-                mapToOppdragsDetaljer,
+                mapToOppdragslinjerWithoutFluff,
             )
         }
     }
@@ -202,28 +150,17 @@ class AttestasjonRepository(
         )
     }
 
-    private val mapToOppdragsDetaljer: (Row) -> OppdragsDetaljer = { row ->
-        OppdragsDetaljer(
-            ansvarsStedForOppdrag = row.stringOrNull("ANSVARSSTEDFOROPPDRAG"),
-            ansvarsStedForOppdragsLinje = row.stringOrNull("ANSVARSSTEDFORLINJE"),
-            antallAttestanter = row.int("ANT_ATTESTANTER"),
-            attestant = row.stringOrNull("ATTESTANT_ID"),
-            datoUgyldigFom = row.stringOrNull("DATO_UGYLDIG_FOM"),
-            datoVedtakFom = row.string("DATO_VEDTAK_FOM"),
-            datoVedtakTom = row.stringOrNull("DATO_VEDTAK_TOM"),
-            delytelsesId = row.string("DELYTELSE_ID"),
-            fagSystemId = row.string("FAGSYSTEM_ID"),
-            kodeFagOmraade = row.string("KODE_FAGOMRAADE"),
+    private val mapToOppdragslinjerWithoutFluff: (Row) -> OppdragslinjeWithoutFluff = { row ->
+        OppdragslinjeWithoutFluff(
+            oppdragsId = row.int("OPPDRAGS_ID"),
+            linjeId = row.int("LINJE_ID"),
             kodeKlasse = row.string("KODE_KLASSE"),
-            kostnadsStedForOppdrag = row.string("KOSTNADSSTEDFOROPPDRAG"),
-            kostnadsStedForOppdragsLinje = row.stringOrNull("KOSTNADSSTEDFORLINJE"),
-            linjeId = row.string("LINJE_ID"),
-            fagGruppe = row.string("NAVN_FAGGRUPPE"),
-            fagOmraade = row.string("NAVN_FAGOMRAADE"),
-            gjelderId = row.string("OPPDRAG_GJELDER_ID"),
-            oppdragsId = row.string("OPPDRAGS_ID"),
+            datoVedtakFom = row.localDate("DATO_VEDTAK_FOM"),
+            datoVedtakTom = row.localDate("DATO_VEDTAK_TOM"),
+            attestert = row.boolean("ATTESTERT"),
             sats = row.double("SATS"),
-            satstype = row.string("TYPE_SATS"),
+            typeSats = row.string("TYPE_SATS"),
+            delytelseId = row.int("DELYTELSE_ID"),
         )
     }
 
--- src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/service/AttestasjonServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/service/AttestasjonServiceTest.kt
@@ -9,12 +9,14 @@ import io.mockk.mockk
 import no.nav.sokos.oppdrag.TestUtil.tokenWithNavIdent
 import no.nav.sokos.oppdrag.attestasjon.api.model.AttestasjonLinje
 import no.nav.sokos.oppdrag.attestasjon.api.model.AttestasjonRequest
+import no.nav.sokos.oppdrag.attestasjon.domain.OppdragslinjeWithoutFluff
 import no.nav.sokos.oppdrag.attestasjon.repository.AttestasjonRepository
 import no.nav.sokos.oppdrag.attestasjon.service.zos.PostOSAttestasjonResponse200
 import no.nav.sokos.oppdrag.attestasjon.service.zos.PostOSAttestasjonResponse200OSAttestasjonOperationResponse
 import no.nav.sokos.oppdrag.attestasjon.service.zos.PostOSAttestasjonResponse200OSAttestasjonOperationResponseAttestasjonskvittering
 import no.nav.sokos.oppdrag.attestasjon.service.zos.PostOSAttestasjonResponse200OSAttestasjonOperationResponseAttestasjonskvitteringResponsAttestasjon
 import no.nav.sokos.oppdrag.attestasjon.service.zos.ZOSConnectService
+import java.time.LocalDate
 
 private val applicationCall = mockk<ApplicationCall>()
 private val attestasjonRepository = mockk<AttestasjonRepository>()
@@ -68,4 +70,75 @@ internal class AttestasjonServiceTest : FunSpec({
         coEvery { zosConnectService.attestereOppdrag(any(), any()) } returns response
         attestasjonService.attestereOppdrag(applicationCall, request) shouldBe response
     }
+
+    test("tilrettelegging av oppdragslinjer for attestering 1") {
+        every { attestasjonRepository.getOppdragslinjerWithoutFluff(any()) } returns
+            withoutFluff(
+                """
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                |OPPDRAGS_ID|LINJE_ID|KODE_KLASSE                                       |DATO_VEDTAK_FOM|DATO_VEDTAK_TOM|ATTESTERT|SATS    |TYPE_SATS|DELYTELSE_ID                  |
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                |58308587   |6       |UFOREUT                                           |2021-12-01     |2021-12-31     |N        |23867.00|MND      |759868829                     |
+                |58308587   |7       |UFOREUT                                           |2022-01-01     |2022-01-31     |J        |23867.00|MND      |759868830                     |
+                |58308587   |8       |UFOREUT                                           |2022-02-01     |2022-04-30     |J        |21816.00|MND      |759868831                     |
+                |58308587   |9       |UFOREUT                                           |2022-05-01     |null           |J        |22857.00|MND      |759868832                     |
+                |58308587   |10      |UFOREUT                                           |2023-01-01     |null           |J        |22857.00|MND      |771253891                     |
+                |58308587   |11      |UFOREUT                                           |2023-05-01     |null           |N        |24322.00|MND      |801686775                     |
+                |58308587   |12      |UFOREUT                                           |2024-01-01     |null           |J        |24322.00|MND      |839410565                     |
+                |58308587   |13      |UFOREUT                                           |2024-05-01     |null           |N        |25431.00|MND      |861989539                     |
+                |58308587   |14      |UFOREUT                                           |2024-07-01     |null           |N        |25899.00|MND      |878336072                     |
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                """.trimIndent(),
+            )
+
+        attestasjonService.getOppdragsDetaljer(12345678) shouldBe emptyList()
+    }
+
+    test("tilrettelegging av oppdragslinjer for attestering") {
+        every { attestasjonRepository.getOppdragslinjerWithoutFluff(any()) } returns
+            withoutFluff(
+                """
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                |OPPDRAGS_ID|LINJE_ID|KODE_KLASSE                                       |DATO_VEDTAK_FOM|DATO_VEDTAK_TOM|ATTESTERT|SATS    |TYPE_SATS|DELYTELSE_ID                  |
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                |63308761   |2       |PENAPGP                                           |2022-12-01     |null           |J        |2005.00 |MND      |756727211                     |
+                |63308761   |8       |PENAPGP                                           |2023-04-01     |null           |J        |2025.00 |MND      |786995290                     |
+                |63308761   |10      |PENAPGP                                           |2023-05-01     |null           |J        |2162.00 |MND      |795307286                     |
+                |63308761   |14      |PENAPGP                                           |2024-02-01     |null           |J        |2185.00 |MND      |839618861                     |
+                |63308761   |16      |PENAPGP                                           |2024-05-01     |null           |J        |2255.00 |MND      |863138304                     |
+                |63308761   |1       |PENAPIP                                           |2022-12-01     |null           |N        |16524.00|MND      |756727209                     |
+                |63308761   |7       |PENAPIP                                           |2023-04-01     |null           |J        |17125.00|MND      |786995288                     |
+                |63308761   |11      |PENAPIP                                           |2023-05-01     |null           |J        |18284.00|MND      |795307289                     |
+                |63308761   |13      |PENAPIP                                           |2024-02-01     |null           |J        |18945.00|MND      |839618859                     |
+                |63308761   |18      |PENAPIP                                           |2024-05-01     |null           |J        |19553.00|MND      |863138307                     |
+                |63308761   |3       |PENAPTP                                           |2022-12-01     |null           |J        |5731.00 |MND      |756727212                     |
+                |63308761   |9       |PENAPTP                                           |2023-04-01     |null           |J        |5799.00 |MND      |786995291                     |
+                |63308761   |12      |PENAPTP                                           |2023-05-01     |null           |J        |6191.00 |MND      |795307288                     |
+                |63308761   |15      |PENAPTP                                           |2024-02-01     |null           |J        |6256.00 |MND      |839618862                     |
+                |63308761   |17      |PENAPTP                                           |2024-05-01     |null           |J        |6457.00 |MND      |863138305                     |
+                +-----------+--------+--------------------------------------------------+---------------+---------------+---------+--------+---------+------------------------------+
+                """.trimIndent(),
+            )
+
+        attestasjonService.getOppdragsDetaljer(12345678) shouldBe emptyList()
+    }
 })
+
+private fun withoutFluff(pretty: String): List<OppdragslinjeWithoutFluff> {
+    return pretty.split("\n").filter { s -> s.isNotBlank() && !s.contains("-----") && !s.contains("OPPDRAGS_ID") }
+        .map { s -> mapToOppdragslinjeWithoutFluff(s.split("|").map { s -> s.trim() }.toList()) }
+}
+
+private fun mapToOppdragslinjeWithoutFluff(params: List<String>): OppdragslinjeWithoutFluff {
+    return OppdragslinjeWithoutFluff(
+        oppdragsId = params.get(1).toInt(),
+        linjeId = params.get(2).toInt(),
+        kodeKlasse = params.get(3),
+        datoVedtakFom = LocalDate.parse(params.get(4)),
+        datoVedtakTom = if (params.get(5).equals("null")) null else LocalDate.parse(params.get(5)),
+        attestert = (if (params.get(6).equals("J")) true else false),
+        sats = params.get(7).toDouble(),
+        typeSats = params.get(8),
+        delytelseId = params.get(9).toInt(),
+    )
+}
