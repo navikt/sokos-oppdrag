6d2c709dd1173fb9bb9bb1bcc1d5a86aac4053b5
Lars Hartvigsen <lars.hartvigsen@nav.no>
2024-09-16 13:53:13 +0200

    Legger inn avtalt skrivemåte for fagSystem

--- src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/attestasjon/repository/AttestasjonRepository.kt
@@ -57,7 +57,7 @@ class AttestasjonRepository(
                     """.trimIndent(),
                 )
             if (gjelderId.isNotBlank()) statementParts.add("AND O.OPPDRAG_GJELDER_ID = :GJELDERID")
-            if (fagSystemId.isNotBlank()) statementParts.add("AND O.FAGSYSTEM_ID LIKE '$fagSystemId%'")
+            if (fagSystemId.isNotBlank()) statementParts.add("AND O.FAGSYSTEM_ID = :FAGSYSTEMID")
             if (kodeFagomraade.isNotBlank()) statementParts.add("AND F.KODE_FAGOMRAADE = :KODEFAGOMRAADE")
             if (kodeFaggruppe.isNotBlank()) statementParts.add("AND G.KODE_FAGGRUPPE = :KODEFAGGRUPPE")
 
@@ -77,6 +77,7 @@ class AttestasjonRepository(
                     statementParts.joinToString("\n", "", ";"),
                     mapOf(
                         "GJELDERID" to gjelderId,
+                        "FAGSYSTEMID" to fagSystemId,
                         "KODEFAGOMRAADE" to kodeFagomraade,
                         "KODEFAGGRUPPE" to kodeFaggruppe,
                         "ATTESTERT" to attestert,
