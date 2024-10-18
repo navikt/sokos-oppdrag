6ddfd25ae69638ad11718844db35ebf282923be4
mudasar187 <mudasar_cheema@hotmail.com>
2024-10-18 13:44:33 +0200

    legger til en test for å sjekke hvis oppdragId ikke finnes i oppdragsinfoservice når vi henter egenskaper

--- src/test/kotlin/no/nav/sokos/oppdrag/oppdragsinfo/service/OppdragsInfoServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/oppdragsinfo/service/OppdragsInfoServiceTest.kt
@@ -1,12 +1,12 @@
 package no.nav.sokos.oppdrag.oppdragsinfo.service
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
 import no.nav.sokos.oppdrag.TestUtil.navIdent
 import no.nav.sokos.oppdrag.oppdragsinfo.domain.Oppdrag
-import no.nav.sokos.oppdrag.oppdragsinfo.domain.OppdragsLinje
 import no.nav.sokos.oppdrag.oppdragsinfo.repository.OppdragsInfoRepository
 
 private val oppdragsInfoRepository = mockk<OppdragsInfoRepository>()
@@ -14,7 +14,8 @@ private val oppdragsInfoService = OppdragsInfoService(oppdragsInfoRepository)
 
 internal class OppdragsInfoServiceTest : FunSpec({
 
-    test("hent liste av oppdragsegenskaper") {
+    test("hent liste av oppdragsegenskaper hvis oppdragId finnes") {
+
         val oppdragList =
             listOf(
                 Oppdrag(
@@ -33,38 +34,15 @@ internal class OppdragsInfoServiceTest : FunSpec({
 
         val result = oppdragsInfoService.getOppdrag("12345678901", "", navIdent)
 
-        result.size shouldBe 1
+        result shouldBe oppdragList
     }
 
-    test("hent liste av oppdragslinjer") {
+    test("hent liste av oppdragsegenskaper hvis oppdragId ikke finnes") {
 
-        val oppdragsId = 12345
+        every { oppdragsInfoRepository.getOppdragId(any()) } returns null
 
-        val oppdragsLinjeList =
-            listOf(
-                OppdragsLinje(
-                    linjeId = 11,
-                    kodeKlasse = "ABC",
-                    datoVedtakFom = "2024-01-01",
-                    datoVedtakTom = null,
-                    sats = 99.9,
-                    typeSats = "DAG",
-                    kodeStatus = "X",
-                    datoFom = "2024-01-01",
-                    linjeIdKorr = 22,
-                    attestert = "J",
-                    delytelseId = "D3",
-                    utbetalesTilId = "A1B2",
-                    refunderesOrgnr = "123456789",
-                    brukerId = "abc123",
-                    tidspktReg = "2024-01-01",
-                ),
-            )
+        val result = oppdragsInfoService.getOppdrag("12345678901", "", navIdent)
 
-        every { oppdragsInfoRepository.getOppdragsLinjer(any()) } returns oppdragsLinjeList
-
-        val result = oppdragsInfoService.getOppdragsLinjer(oppdragsId)
-
-        result.size shouldBe 1
+        result.shouldBeEmpty()
     }
 })
