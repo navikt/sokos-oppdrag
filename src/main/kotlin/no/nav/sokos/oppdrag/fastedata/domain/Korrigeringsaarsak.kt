e04602ed394f031b944a7a0c0ac9409418132ffd
Lars Hartvigsen <lars.hartvigsen@nav.no>
2025-01-16 12:28:27 +0100

    Gjør korrigeringsårsaker serializable

--- src/main/kotlin/no/nav/sokos/oppdrag/fastedata/domain/Korrigeringsaarsak.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/fastedata/domain/Korrigeringsaarsak.kt
@@ -1,5 +1,8 @@
 package no.nav.sokos.oppdrag.fastedata.domain
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Korrigeringsaarsak(
     val navn: String,
     val kode: String,
