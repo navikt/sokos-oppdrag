c42682668620bda856657b9b8b1a268065fa1853
mudasar187 <mudasar_cheema@hotmail.com>
2025-01-27 10:20:02 +0100

    mer realistisk test data

--- src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/Testdata.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/attestasjon/Testdata.kt
@@ -1,6 +1,8 @@
 package no.nav.sokos.oppdrag.attestasjon
 
 import no.nav.sokos.oppdrag.TestUtil.readFromResource
+import no.nav.sokos.oppdrag.attestasjon.api.model.AttestasjonLinje
+import no.nav.sokos.oppdrag.attestasjon.api.model.AttestasjonRequest
 import no.nav.sokos.oppdrag.attestasjon.api.model.AttestertStatus.ALLE
 import no.nav.sokos.oppdrag.attestasjon.api.model.OppdragsRequest
 import no.nav.sokos.oppdrag.attestasjon.domain.Oppdrag
@@ -23,16 +25,16 @@ object Testdata {
     private val oppdragTestdata =
         Oppdrag(
             antAttestanter = 1,
-            navnFaggruppe = "fagGruppe",
-            navnFagomraade = "fagOmraade",
-            fagSystemId = "fagSystemId",
+            navnFaggruppe = "Arbeidsytelser",
+            navnFagomraade = "Arbeidsavklaringspenger",
+            fagSystemId = "123456789",
             oppdragGjelderId = GJELDER_ID,
-            kodeFaggruppe = "ERSEPT",
-            kodeFagomraade = "MOSALLE",
-            kostnadssted = "kostnadsSted",
-            ansvarssted = "ansvarsSted",
-            oppdragsId = 1,
-            attestanter = mutableMapOf(1 to listOf("attestant1")),
+            kodeFaggruppe = "ARBYT",
+            kodeFagomraade = "AAP",
+            kostnadssted = "8020",
+            ansvarssted = "4819",
+            oppdragsId = 58308587,
+            attestanter = mutableMapOf(1 to listOf(navIdent.ident)),
         )
 
     val oppdragDTOTestdata = oppdragTestdata.toDTO(hasWriteAccess = true)
@@ -44,5 +46,14 @@ object Testdata {
             kodeFagGruppe = null,
             kodeFagOmraade = null,
             attestertStatus = ALLE,
+        )
+
+    val attestasjonRequestTestdata =
+        AttestasjonRequest(
+            GJELDER_ID,
+            "123456789",
+            "AAP",
+            58308587,
+            listOf(AttestasjonLinje(1, navIdent.ident, "2021-01-01")),
         )
 }
