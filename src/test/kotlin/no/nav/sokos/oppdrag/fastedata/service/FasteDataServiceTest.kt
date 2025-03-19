3b972f9540a5c115a04b5c36c38295209eda7070
Sebabebab <smedsrudsebastian@gmail.com>
2025-03-19 08:55:48 +0100

    Laget service test for ventestatuskoder

--- src/test/kotlin/no/nav/sokos/oppdrag/fastedata/service/FasteDataServiceTest.kt
+++ src/test/kotlin/no/nav/sokos/oppdrag/fastedata/service/FasteDataServiceTest.kt
@@ -13,33 +13,50 @@ import no.nav.sokos.oppdrag.listener.Db2Listener.fasteDataFagomraadeRepository
 import no.nav.sokos.oppdrag.listener.Db2Listener.venteKriterierRepository
 import no.nav.sokos.oppdrag.listener.Db2Listener.ventestatuskodeRepository
 
-internal class FasteDataServiceTest :
-    FunSpec({
-        extensions(Db2Listener)
+internal class FasteDataServiceTest : FunSpec({
+    extensions(Db2Listener)
 
-        val fastedataService =
-            FasteDataService(
-                fasteDataFagomraadeRepository,
-                venteKriterierRepository,
-                ventestatuskodeRepository,
-            )
+    val fastedataService =
+        FasteDataService(
+            fasteDataFagomraadeRepository,
+            venteKriterierRepository,
+            ventestatuskodeRepository,
+        )
 
-        test("getAllVentekriterier skal returnere en liste av Ventekriterier") {
-            Db2Listener.dataSource.transaction { session ->
-                session.update(queryOf("database/fastedata/getVentekriterier.sql".readFromResource())) shouldBeGreaterThan 0
-            }
-
-            val result = fastedataService.getAllVentekriterier()
-            result.shouldNotBeEmpty()
-            result.size shouldBe 2
-
-            val ventekriterier = result.first()
-            ventekriterier.kodeFaggruppe shouldBe "GH"
-            ventekriterier.typeBilag shouldBe "O"
-            ventekriterier.datoFom shouldBe "1900-01-01"
-            ventekriterier.belopBrutto shouldBe null
-            ventekriterier.belopNetto shouldBe 100000.00
-            ventekriterier.antDagerEldreenn shouldBe null
-            ventekriterier.tidligereAar shouldBe false
+    test("getAllVentekriterier skal returnere en liste av Ventekriterier") {
+        Db2Listener.dataSource.transaction { session ->
+            session.update(queryOf("database/fastedata/getVentekriterier.sql".readFromResource())) shouldBeGreaterThan 0
         }
-    })
+
+        val result = fastedataService.getAllVentekriterier()
+        result.shouldNotBeEmpty()
+        result.size shouldBe 2
+
+        val ventekriterier = result.first()
+        ventekriterier.kodeFaggruppe shouldBe "GH"
+        ventekriterier.typeBilag shouldBe "O"
+        ventekriterier.datoFom shouldBe "1900-01-01"
+        ventekriterier.belopBrutto shouldBe null
+        ventekriterier.belopNetto shouldBe 100000.00
+        ventekriterier.antDagerEldreenn shouldBe null
+        ventekriterier.tidligereAar shouldBe false
+    }
+
+    test("getAllVentestatuskoder skal returnere en liste av Ventestatuskode") {
+        Db2Listener.dataSource.transaction { session ->
+            session.update(queryOf("database/fastedata/getVentestatuskoder.sql".readFromResource())) shouldBeGreaterThan 0
+        }
+
+        val result = fastedataService.getAllVentestatuskoder()
+        result.shouldNotBeEmpty()
+        result.size shouldBe 2
+
+        val ventestatuskode = result.first()
+        ventestatuskode.kodeVentestatus shouldBe "ADDR"
+        ventestatuskode.beskrivelse shouldBe "Periode ikke utbet, navn/adresse mangler"
+        ventestatuskode.prioritet shouldBe 120
+        ventestatuskode.settesManuelt shouldBe true
+        ventestatuskode.kodeArvesTil shouldBe "ADDR"
+        ventestatuskode.kanManueltEndresTil shouldBe "AVVE"
+    }
+})
