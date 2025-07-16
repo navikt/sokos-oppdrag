c100b91812e62527366a88c85f2a3b8833e9df51
takwaiwang <tak.wai.wang@nav.no>
2025-07-16 14:59:53 +0200

    Fiks netty restart problem

--- src/main/kotlin/no/nav/sokos/oppdrag/Application.kt
+++ src/main/kotlin/no/nav/sokos/oppdrag/Application.kt
@@ -1,6 +1,10 @@
 package no.nav.sokos.oppdrag
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.Application
+import io.ktor.server.config.HoconApplicationConfig
+import io.ktor.server.engine.applicationEnvironment
+import io.ktor.server.engine.connector
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
@@ -12,7 +16,31 @@ import no.nav.sokos.oppdrag.config.routingConfig
 import no.nav.sokos.oppdrag.config.securityConfig
 
 fun main() {
-    embeddedServer(Netty, port = 8080, module = Application::module).start(true)
+    val applicationEngineEnvironment =
+        applicationEnvironment {
+            config = HoconApplicationConfig(ConfigFactory.load())
+        }
+
+    val server =
+        embeddedServer(
+            factory = Netty,
+            environment = applicationEngineEnvironment,
+            configure = {
+                connector {
+                    port = 8080
+                }
+                shutdownGracePeriod = 2000
+                shutdownTimeout = 10000
+            },
+            module = Application::module,
+        )
+
+    Runtime.getRuntime().addShutdownHook(
+        Thread {
+            server.stop(2000, 10000)
+        },
+    )
+    server.start(true)
 }
 
 fun Application.module() {
