bc1e7db7b3872ab35a16c7868b5cf7830c5ff67f
Jonas Nicolaysen <jonas.nicolaysen@nav.no>
2026-05-07 13:39:41 +0200

    oppdaterer netty pga sårbarhet

--- build.gradle.kts
+++ build.gradle.kts
@@ -177,11 +177,11 @@ configurations.all {
                 because("jackson-core: Number Length Constraint Bypass in Async Parser Leads to Potential DoS Condition. Affected version >= 2.19.0, < 2.21.1")
             }
             if (requested.group == "io.netty" && requested.name == "netty-codec-http") {
-                useVersion("4.2.11.Final")
+                useVersion("4.2.13.Final")
                 because("Netty: HTTP Request Smuggling via Chunked Extension Quoted-String Parsing. Affected version >= 4.2.0.Alpha1, < 4.2.10.Final")
             }
             if (requested.group == "io.netty" && requested.name == "netty-codec-http2") {
-                useVersion("4.2.11.Final")
+                useVersion("4.2.13.Final")
                 because("Netty HTTP/2 CONTINUATION Frame Flood DoS via Zero-Byte Frame Bypass. Affected version >= 4.2.0.Alpha1, < 4.2.10.Final")
             }
             if (requested.group == "org.bouncycastle" && requested.name == "bcprov-jdk18on") {
